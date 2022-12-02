ce9c0203cbf0c27ec24169ed1fb8932f3b36a9da
NguyenThePhong <nguyenthephong_t65@hus.edu.vn>
2022-12-02 14:45:41 +0700

    router final

--- src/store/modules/authorization/auth.js
+++ src/store/modules/authorization/auth.js
@@ -3,61 +3,60 @@ import { LOGIN, REGISTER, LOGOUT, REFRESH } from "@/constants/index"; // biến 
 import { removeAuthenticated } from "@/utils/auth"; // xoas key localStorage == Logout
 
 const auth = {
-	namespaced: true,
-	state: {
-		user: "",
-	},
-	getters: {
-		getUserProfile: (state) => state.user.profile,
-		getUserName: (state) => state.user.profile.name,
-		getUserEmail: (state) => state.user.profile.email,
-		getUserRole: (state) => state.user.profile.name,
-		getUserId: (state) => state.user.profile.id,
-		getUserPassword: (state) => state.user.profile.password,
-	},
-	mutations: {
-		[LOGIN](state, { user }) {
-			state.user = user;
-		},
-		[LOGOUT](state) {
-			state.user = "";
-		},
-		[REFRESH](state, { token }) {
-			state.user.token = token;
-		},
-	},
-	actions: {
-		async getLogin({ commit }, { email, password }) {
-			const data = await authService.login({ email, password });
-			if (data && data.token && data.user) {
-				commit(LOGIN, {
-					user: { token: data.token, profile: data.user },
-				});
-			}
-		},
-		getLogout({ commit }) {
-			console.log("logout");
-			authService.logout().then((res) => {
-				console.log(res);
-				commit(LOGOUT);
-				removeAuthenticated();
-				// this.state.user.token = "";
-				return res;
-			});
-		},
-		register({ email, password, name, c_password }) {
-			console.log(email, password, name, c_password);
-			return authService.register({ email, password, name, c_password });
-		},
+    namespaced: true,
+    state: {
+        user: "",
+    },
+    getters: {
+        getUserProfile: (state) => state.user.profile,
+        getUserName: (state) => state.user.profile.name,
+        getUserEmail: (state) => state.user.profile.email,
+        getUserRole: (state) => state.user.profile.name,
+        getUserId: (state) => state.user.profile.id,
+        getUserPassword: (state) => state.user.profile.password,
+    },
+    mutations: {
+        [LOGIN](state, { user }) {
+            state.user = user;
+        },
+        [LOGOUT](state) {
+            state.user = "";
+        },
+        [REFRESH](state, { token }) {
+            state.user.token = token;
+        },
+    },
+    actions: {
+        async getLogin({ commit }, { email, password }) {
+            const data = await authService.login({ email, password });
+            if (data && data.token && data.user) {
+                commit(LOGIN, {
+                    user: { token: data.token, profile: data.user },
+                });
+            }
+        },
+        getLogout({ commit }) {
+            authService.logout().then((res) => {
+                console.log(res);
+                commit(LOGOUT);
+                removeAuthenticated();
+                // this.state.user.token = "";
+                return res;
+            });
+        },
+        register({ email, password, name, c_password }) {
+            console.log(email, password, name, c_password);
+            return authService.register({ email, password, name, c_password });
+        },
 
-		[REFRESH]({ commit }) {
-			return authService.refresh().then((data) => {
-				if (data && data.token) {
-					commit(REFRESH, { token: data.token });
-				}
-			});
-		},
-	},
+        [REFRESH]({ commit }) {
+            return authService.refresh().then((data) => {
+                if (data && data.token) {
+                    commit(REFRESH, { token: data.token });
+                }
+            });
+        },
+    },
 };
 
 export default auth;
